4f5e965918de69fe024f01a2c0a6031aa50caf58
Chaitanya Kukde <10692792+cha-ku@users.noreply.github.com>
2026-09-06 10:05:34 +0100

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        std::stack<char> brackets;
+        for (int i = 0; i < s.size(); ++i) {
+            const char b = s[i];
+            if (brackets.empty() ||  b == '(' || b == '[' || b == '{' ) {
+                brackets.push(b);
+                continue;
+            }
+            if ((b == ')' && brackets.top() == '(') || (b == ']' && brackets.top() == '[') ||(b == '}' && brackets.top() == '{')) {
+                brackets.pop();
+            }
+            else {
+                brackets.push(b);
+            }
+        }
+        return brackets.empty();
+    }
+};
